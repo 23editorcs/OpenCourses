a7b0ac5919fa6265e6bd2f79969a8f1378d9aac2
23editorcs <23editorcs@gmail.com>
2017-10-05 11:54:42 +0700

    Application Redo

--- 000_DeepLearning/04_Image_Classification/dnn_app_utils_v2.py
+++ 000_DeepLearning/04_Image_Classification/dnn_app_utils_v2.py
@@ -424,13 +424,15 @@ def print_mislabeled_images(classes, X, y, p):
     p -- predictions
     """
     a = p + y
+    #print(a.shape)
     mislabeled_indices = np.asarray(np.where(a == 1))
+    #print(mislabeled_indices)
     plt.rcParams['figure.figsize'] = (40.0, 40.0) # set default size of plots
     num_images = len(mislabeled_indices[0])
     for i in range(num_images):
         index = mislabeled_indices[1][i]
         
-        plt.subplot(2, num_images, i + 1)
+        plt.subplot(num_images/2, 2 , i + 1)
         plt.imshow(X[:,index].reshape(64,64,3), interpolation='nearest')
         plt.axis('off')
         plt.title("Prediction: " + classes[int(p[0,index])].decode("utf-8") + " \n Class: " + classes[y[0,index]].decode("utf-8"))
