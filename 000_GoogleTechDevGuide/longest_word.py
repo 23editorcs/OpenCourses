a9838b8a5e7267b8379954be6a0d4a5fc6d2caa6
23editorcs <23editorcs@gmail.com>
2017-11-10 12:17:07 +0700

    longest word python

--- 000_GoogleTechDevGuide/longest_word.py
+++ 000_GoogleTechDevGuide/longest_word.py
@@ -2,12 +2,49 @@
 '''
 Fing longest word in dictionary that is a subsequence of a given string
 '''
+import collections
+import sys
 
-def longest_word(s, d):
+import subdict as subdict
+
+
+def longest_word(letters, words):
     '''
     s is a given string which word is should be subsequence of s
     d is a dictionary which could be any format (list, hash table, prefix tree...
-    '''
-    # greedy algorithm solution
+        '''
+    letter_positions = collections.defaultdict(list)
+    # For each letter in 'letters', collect all the indices at which it appears
+    # O(#letters) space and speed
+    for index, letter in enumerate(letters):
+        letter_positions[letter].append(index)
+    #For words, in descending order by length...
+    # Bails out early on first match word
+    for word in sorted(words, key=lambda w: len(w), reverse=True):
+        pos = 0
+        for letter in word:
+            if letter not in letter_positions:
+                break
+
+            possible_positions = [p for p in letter_positions[letter] if p >= pos]
+            print(possible_positions)
+            if not possible_positions:
+                break
+            pos = possible_positions[0] + 1
+        else:
+            return word
+'''
+if __name__ == '__main__':
+    print(longest_word("abppplee", {"able", "ale", "apple", "bale", "kangaroo"}))
+'''
+print(longest_word("abppplee", {"able", "ale", "apple", "bale", "kangaroo"}))
+
+            
+
+
+
+
+            
                 
+
 
