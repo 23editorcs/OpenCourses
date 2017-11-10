55c98d29d088ad46d0fd616f96f71a0d1400f167
23editorcs <23editorcs@gmail.com>
2017-11-10 11:13:38 +0700

    Google tech

---
+++ 000_GoogleTechDevGuide/longest_word.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''
+Fing longest word in dictionary that is a subsequence of a given string
+'''
+
+def longest_word(s, d):
+    '''
+    s is a given string which word is should be subsequence of s
+    d is a dictionary which could be any format (list, hash table, prefix tree...
+    '''
+    # greedy algorithm solution
+                
+
