4f927bc70da9e1cef0ef94640674d75820a6850d
23editorcs <23editorcs@gmail.com>
2017-12-11 13:42:42 +0700

    Finished Inventory

--- 006_SoftwareConstruction_OOD/09_design-patterns-lecture-starters-master/Iterator-ToyStoreAdvancedStarter/src/model/Inventory.java
+++ 006_SoftwareConstruction_OOD/09_design-patterns-lecture-starters-master/Iterator-ToyStoreAdvancedStarter/src/model/Inventory.java
@@ -2,9 +2,10 @@ package model;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
 
 
-public class Inventory {
+public class Inventory implements Iterable<Toy> {
     private Collection<Toy> toys = new ArrayList<>();
     private Collection<Toy> backlog = new ArrayList<>();
     private ArrayList<String> log = new ArrayList<>();
@@ -35,4 +36,32 @@ public class Inventory {
     }
 
 
+    @Override
+    public Iterator<Toy> iterator() {
+        return new ToyIterator();
+    }
+
+    private class ToyIterator implements Iterator<Toy> {
+
+        private Iterator toyIterator = toys.iterator();
+        private Iterator logIterator = backlog.iterator();
+
+        @Override
+        public boolean hasNext() {
+            System.out.println("Must has next!");
+            return toyIterator.hasNext() || logIterator.hasNext();
+        }
+
+        @Override
+        public Toy next() {
+            if (toyIterator.hasNext()) {
+                System.out.println("Toy!!");
+                return (Toy) toyIterator.next();
+            }
+            else {
+                System.out.println("Log!!");
+                return (Toy) logIterator.next();
+            }
+        }
+    }
 }
