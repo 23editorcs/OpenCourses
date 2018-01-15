905d7eafcd924dde3292816aba807c891e062994
23editorcs <23editorcs@gmail.com>
2018-01-15 15:36:38 +0700

    Finished HW4

--- 008_ProgrammingLanguageB/HW4/hw4.rkt
+++ 008_ProgrammingLanguageB/HW4/hw4.rkt
@@ -28,3 +28,54 @@
   (if (= n 0)
       null
       (cons (car (s)) (stream-for-n-steps (cdr (s)) (- n 1)))))
+
+(define funny-number-stream
+  (letrec ([f (lambda (x) (if (= (remainder x 5) 0)
+                              (cons (- x) (lambda () (f (+ x 1))))
+                              (cons x (lambda () (f (+ x 1))))))])
+    (lambda () (f 1))))
+
+(define dan-then-dog
+  (letrec ([x "dan.jpg"]
+           [y "dog.jpg"]
+           [f1 (lambda () (cons x (lambda () (f2))))]
+           [f2 (lambda () (cons y (lambda () (f1))))])
+    (lambda () (f1))))
+
+(define (stream-add-zero s)
+  (lambda () (cons (cons 0 (car (s))) (stream-add-zero (cdr (s))))))
+
+(define (cycle-lists xs ys)
+  (letrec ([f (lambda (n) 
+                (cons (cons (list-nth-mod xs n) (list-nth-mod ys n))
+                      (lambda () (f (+ n 1)))))])
+    (lambda () (f 0))))
+
+(define (vector-assoc v vec)
+  (letrec ([f (lambda (x) (if (> x (vector-length vec))
+                              #f
+                              (if (equal? v (car (vector-ref vec x)))
+                                  (vector-ref vec x)
+                                  (f (+ x 1)))))])
+    (f 0)))
+
+(define (cached-assoc xs n)
+  (letrec ([cached (make-vector n #f)]
+           [y 0]
+           [f (lambda (v)
+                (let ([ans (if (vector-ref cached (- n 1))
+                               (vector-assoc v cached)
+                               #f)])
+                  (if ans
+                      ans
+                      (let ([new-ans (assoc v xs)])
+                        (if new-ans
+                            (begin (vector-set! cached y new-ans)
+                                   (set! y (+ y 1))
+                                   new-ans)
+                            new-ans)))))])
+    f))
+                        
+                                     
+                        
+                    
